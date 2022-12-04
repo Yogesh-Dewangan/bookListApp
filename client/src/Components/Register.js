76976362c8070fdc7d85627bea32930963540482
Yogesh Dewangan <yogdev0dew@gmail.com>
2022-12-04 19:06:18 +0530

    handled error comming from server side to utilise in frontend

--- client/src/Components/Register.js
+++ client/src/Components/Register.js
@@ -8,14 +8,31 @@ export default function Register() {
         password:'',
         cpassword:''
     })
+    const [error, setError] = useState('')
     const navigate = useNavigate();
 
     const submitHandler = (e) => {
         e.preventDefault();
+        setError('');
         registerUser(user)
             .then(res => {
-                console.log(res);
-                // navigate('/'); 
+                // console.log(res);
+                if(res.status === "Success") {
+                    alert(res.message);
+                    navigate('/');
+                } else {
+                    if(res.status === 'Failed') {
+                        setError(res.message);
+                    } else {
+                        if (res.errors[0].param === 'username') {
+                            setError('Username has to be email id of user')
+                        } else if (res.errors[0].param === 'password') {
+                            setError('password length must be atleast 6 and atmost 16')
+                        } else {
+                            setError(res.error);
+                        }
+                    }
+                }
             })
             .catch(err => console.log(err))
     }
@@ -25,12 +42,15 @@ export default function Register() {
             <img/>
             <h1 className='text-center my-4 text-white'>Register</h1>
             <form className='d-flex flex-column align-items-center' method='POST' onSubmit={submitHandler}>
+                <div className='text-start'>
+                    <p className='text-warning text-start'>{error}</p>
+                </div>
                 <input 
                     type='email' 
                     name='username' 
                     value={user.username} 
-                    placeholder='Username' 
-                    className='form-control my-3 w-75 bg-light'
+                    placeholder='Username Ex: abc@xyz' 
+                    className='form-control mt-1 mb-3 w-75 bg-light'
                     onChange={(e) => {
                         setUser((user) => ({
                             ...user,
