eabc842f702095c16e6b92b1ed426c34fa6b3cb5
Yogesh Dewangan <yogdev0dew@gmail.com>
2022-12-04 18:59:51 +0530

    added signInUser, getBookList, addBook apis

--- client/src/Components/apiUtils.js
+++ client/src/Components/apiUtils.js
@@ -10,3 +10,36 @@ export const registerUser = async (user) => {
     })
     return res.json();
 }
+
+export const signInUser = async (user) => {
+    const res = await fetch(`${URL}/v1/signin`, {
+        method: "POST",
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(user)
+    })
+    return res.json();
+}
+
+export const getBookList = async () => {
+    const res = await fetch(`${URL}/v1/books`, {
+        method: 'GET',
+        headers: {
+            'Authorization': localStorage.getItem('token')
+        }
+    })
+    return res.json();
+}
+
+export const addBook = async (book) => {
+    const res = await fetch(`${URL}/v1/books`, {
+        method: "POST",
+        headers: {
+            'Content-Type': 'application/json',
+            'Authorization': localStorage.getItem('token') 
+        },
+        body: JSON.stringify(book)
+    })
+    return res.json();
+}
