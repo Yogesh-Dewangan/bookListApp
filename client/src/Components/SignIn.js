0de7266eb5cef0beab155f45e8e2e05c72e6b737
Yogesh Dewangan <yogdev0dew@gmail.com>
2022-12-04 19:07:31 +0530

    handled error from server to utilise in frontend and moved signInUser api call to apiUtils

--- client/src/Components/SignIn.js
+++ client/src/Components/SignIn.js
@@ -1,44 +1,48 @@
 import {useState} from 'react';
 import {useNavigate, Link} from 'react-router-dom';
+import { signInUser } from './apiUtils';
 
 export default function SignIn() {
     const [user, setUser] = useState({
         username:'',
         password:''
     })
+    const [error, setError] = useState('');
     const navigate = useNavigate();
-
-    const signInUser = async () => {
-        const res = await fetch('/v1/users', {
-            method: "POST",
-            headers: {
-                'Content-Type': 'application/json'
-            },
-            body: JSON.stringify(user)
-        })
-        return res.json();
-    }
 
     const submitHandler = (e) => {
         e.preventDefault();
-        signInUser()
+        signInUser(user)
             .then(res => {
                 console.log(res);
-                navigate('/books'); 
+                if(res.status === 'Success') {
+                    localStorage.setItem('token', res.token)
+                    alert(res.message);
+                    navigate('/booklist');
+                } else {
+                    if(res.status === 'Failed') {
+                        setError(res.message);
+                    } else {
+                        setError(res.error);
+                    }
+                }
             })
             .catch(err => console.log(err))
     }
 
     return <div className='container p-5'>
         <div className='d-grid gap-2 col-5 mx-auto mt-5 bg-secondary'>
-            <img/>
+            {/* <img/> */}
             <h1 className='text-center my-4 text-white'>Member SignIn</h1>
             <form className='d-flex flex-column align-items-center' method='POST' onSubmit={submitHandler}>
+                <div className='text-start'>
+                    <p className='text-warning text-start'>{error}</p>
+                </div>
                 <input 
                     type='email' 
                     name='email' 
                     value={user.username} 
-                    placeholder='Username' 
+                    placeholder='Username Ex: abc@xyz' 
                     className='form-control my-3 w-75 bg-light'
                     onChange={(e) => {
                         setUser((user) => ({
