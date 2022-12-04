534bb6c98b9ef44efe4711767973bfdf07a79fa1
Yogesh Dewangan <yogdev0dew@gmail.com>
2022-12-04 19:08:41 +0530

    modified userName to username in schema

--- server/model/User.js
+++ server/model/User.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const userSchema = new Schema ({
-    userName: {type: String, required: true, unique: true},
+    username: {type: String, required: true, unique: true},
     password: {type: String, required: true}
 });
 
